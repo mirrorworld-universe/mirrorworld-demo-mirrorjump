55ebd580f8879be96cfa13c7f45afa7fcbffb203
Yv Pu <57607739+riomarksafk@users.noreply.github.com>
2022-09-18 22:25:35 +0800

    version save

--- Assets/Scripts/ECS/script/Menu/Options.cs
+++ Assets/Scripts/ECS/script/Menu/Options.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Options : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+
+
+
+    public GameObject OptionMenu;
+
+    public GameObject OptionMenuDetails;
+
+    public GameObject Bg;
+    
+    
+    
+
+
+    public void OpenOptions()
     {
+        Bg.SetActive(true);
+        OptionMenu.SetActive(true);
+        OptionMenuDetails.SetActive(false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitOptions()
     {
-        
+        Bg.SetActive(false);
+        OptionMenu.SetActive(false);
+        OptionMenuDetails.SetActive(false);
     }
+
+
+    public void BackOptionMenu()
+    {  
+        OptionMenuDetails.SetActive(false);
+        OptionMenu.SetActive(true);
+    }
+    
+    
+
+    public void AboutMirrorJump()
+    {
+        OptionMenu.SetActive(false);
+        OptionMenuDetails.SetActive(true);
+    }
+
+    public void OpenFeedback()
+    {
+        Application.OpenURL("https://app-staging.mirrorworld.fun/auth/login");
+    }
+    
+    public void OpenFAQ()
+    {
+        Application.OpenURL("https://developer.mirrorworld.fun/#1696cb75-cb3b-46c0-9d3d-dea7c0f87f74");
+    }
+    
+    public void OpenMirrorSDKLink()
+    {
+        Application.OpenURL("https://mirrorworld.fun/");
+    }
+    
+   
 }

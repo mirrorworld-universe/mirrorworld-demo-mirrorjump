99d2ad11a554ac97e109b8b9076a2403572d6c21
gwc0111 <gwc0111@qq.com>
2022-10-10 18:46:03 +0800

    feat: 不带新场景的新记录逻辑

---
+++ Assets/Scripts/ECS/script/Menu/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // 每次旋转360度的多少份
+    [SerializeField] private int rotateData = 68;
+    private Vector3 eulerAngle;
+
+    // 多久旋转一次
+    [SerializeField] private float changeTime = 0.5f;
+
+    [SerializeField] private float timeRecorder;
+    private void FixedUpdate()
+    {
+        timeRecorder += Time.deltaTime;
+        if(timeRecorder > changeTime)
+        {
+            timeRecorder = 0;
+            eulerAngle = transform.eulerAngles;
+            eulerAngle.z += 360 / rotateData;
+            transform.eulerAngles = eulerAngle;
+        }
+    }
+}

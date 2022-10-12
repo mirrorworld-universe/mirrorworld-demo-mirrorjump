33786799c3603b135d1cb002e30ee86af9ca2970
Yv Pu <57607739+riomarksafk@users.noreply.github.com>
2022-10-12 14:59:55 +0800

    start 生成组需求

--- Assets/Scripts/ECS/script/Game/Group.cs
+++ Assets/Scripts/ECS/script/Game/Group.cs
@@ -1,10 +1,39 @@
 
 
+using System.Collections.Generic;
+
 public class Group
 {
+
+   public string GroupName;
+
+   public float HeightFloor;
+
+   public float HeightUpper;
+
+   public float BaseYOffsetFloor;
+
+   public float BaseYOffsetUpper;
    
-  
-    
-    
-    
+   public float BaseXOffsetFloor;
+
+   public float BaseXOffsetUpper;
+   
+   public int GenerateFloor;
+
+   public int GenerateUpper;
+   
+   
+   public int ExtraYoffsetFloor;
+
+   public int ExtraYoffsetUpper;
+
+
+   public List<int> ExtraTypePercent;
+   
+   public List<int> PropTypePercent;
+   
+   
+   
+
 }
